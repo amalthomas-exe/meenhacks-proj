6ad5ace1c356b294826a9b2546a4660bf3b86e7a
AKA717 <alwinaji717@gmail.com>
2023-04-16 23:09:21 +0530

    routing done from home to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import {Routes , Route } from "react-router-dom"
 import Home from './Components/Home/Home'
+import Header from './Components/Header/header'
 import Form from './Components/Form/Form'
 
 function App() {
 
   return (
     <div className="App">
-       <Form />
+      <Header/>
+        <Routes>
+            <Route path='/' exact element={<Home/>}/>
+            <Route exact path='/form' element={<Form/>}/>
+       </Routes> 
     </div>
   )
 }
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'; 
+import './header.css'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <Link to="/">Home</Link>
+            <Link to="/form">Form</Link>
+        </div>
+    );
+}
+
+export default Header;
